0c4f2aa829871a563bf9bc8c2f6cd17d8f917bed
Ricardo Camilo <ricardo_hks@hotmail.com>
2024-12-25 11:42:57 -0300

    Refactor index.ts to utilize convertSecondsToHours for uptime calculation, comment out unused code, and clean up logging statements

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,34 @@
-import { sayHello } from "@utils/hello.js";
+// import { sayHello } from "@utils/hello.ts";
+import { convertSecondsToHours } from "@utils/convertSecondsToHours.ts";
 import * as os from "os";
 
-const systemUptime = os.uptime();
-const userInfo = os.userInfo();
-const otherInfo = {
-  name: os.type(),
-  release: os.release(),
-  totalMem: os.totalmem(),
-  freeMem: os.freemem(),
-}
+const systemUptime = convertSecondsToHours(os.uptime());
+// const userInfo = os.userInfo();
+// const otherInfo = {
+//   name: os.type(),
+//   release: os.release(),
+//   totalMem: os.totalmem(),
+//   freeMem: os.freemem(),
+// }
 
+// console.log("userInfo:", userInfo);
+// console.log("otherInfo:", otherInfo);
 console.log("systemUptime:", systemUptime);
-console.log("userInfo:", userInfo);
-console.log("otherInfo:", otherInfo);
 
 
-sayHello("Ricardo");
-sayHello("Camillus");
-sayHello("Bloodfallen")
+// sayHello("Ricardo");
+// sayHello("Camillus");
+// sayHello("Bloodfallen")
 
 
-declare global {
-  var myVariable: string;
-}
+// declare global {
+//   var myVariable: string;
+// }
 
-global.myVariable = "Hellow World";
+// global.myVariable = "Hellow World";
 
-console.log("myVariable:", myVariable);
+// console.log("myVariable:", myVariable);
 
-console.log("dirname:", __dirname);
+// console.log("dirname:", __dirname);
 
-console.log("filename:", __filename);
+// console.log("filename:", __filename);
